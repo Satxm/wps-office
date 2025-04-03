a57e398d84abcbb331b02df8743ec1ab6b8a3cd4
Satxm <Satxm@outlook.com>
2025-04-04 00:56:03 +0800

    Add files via upload

--- wpslinux-deb.sh
+++ wpslinux-deb.sh
@@ -26,7 +26,7 @@ rm -rf wps-365/usr/share/doc/*xiezuo*
 rm -rf wps-365/usr/share/fonts
 rm -rf wps-365/usr/share/applications/{wps-office-officeassistant.desktop,wps-office-uninstall.desktop,xiezuo.desktop}
 rm -rf wps-365/usr/bin/{wps_uninstall.sh,wps_xterm}
-rm -rf wps-365/usr/share/desktop-directories
+rm -rf wps-365/opt/kingsoft/wps-office/templates/*
 
 # fix template path
 sed -i 's|URL=.*|URL=/opt/kingsoft/wps-office/office6/mui/zh_CN/templates/newfile.docx|' \
@@ -36,22 +36,43 @@ sed -i 's|URL=.*|URL=/opt/kingsoft/wps-office/office6/mui/zh_CN/templates/newfil
 sed -i 's|URL=.*|URL=/opt/kingsoft/wps-office/office6/mui/zh_CN/templates/newfile.pptx|' \
  wps-365/usr/share/templates/wps-office-wpp-template.desktop
 
+sed -s -i 's|\]=wps|\]=WPS |' \
+ wps-365/usr/share/templates/wps-office-et-template.desktop \
+ wps-365/usr/share/templates/wps-office-wpp-template.desktop \
+ wps-365/usr/share/templates/wps-office-wps-template.desktop
+
+cp -f wps-365/opt/kingsoft/wps-office/office6/mui/zh_CN/templates/newfile.docx wps-365/opt/kingsoft/wps-office/templates/WPS文字文档.docx
+cp -f wps-365/opt/kingsoft/wps-office/office6/mui/zh_CN/templates/newfile.xlsx wps-365/opt/kingsoft/wps-office/templates/WPS表格工作表.xlsx
+cp -f wps-365/opt/kingsoft/wps-office/office6/mui/zh_CN/templates/newfile.pptx wps-365/opt/kingsoft/wps-office/templates/WPS演示文稿.pptx
+
+sed -i 's|金山办公|WPS Office|' \
+ wps-365/etc/xdg/menus/applications-merged/wps-office.menu \
+ wps-365/usr/share/desktop-directories/wps-office.directory
+
+sed -i 's|wps-office-kingsoft|wps-office-kingsoft|' /usr/share/desktop-directories/wps-office.directory
+
 # fix menu category
 sed -i 's|Categories=.*|&Office;|' wps-365/usr/share/applications/*.desktop
 
 # fix background process
 sed -i '2i [[ $(ps -ef | grep -c "office6/$(basename $0)") == 1 ]] && export gOptExt=-multiply' \
- wps-365/usr/bin/{wps,wpp,et,wpspdf}
+ wps-365/usr/bin/wps \
+ wps-365/usr/bin/wpp \
+ wps-365/usr/bin/et \
+ wps-365/usr/bin/wpspdf
 
 sed -i 's|YUA..=NsbhfV4nLv_oZGENyLSVZA..|YUA..=WHfH10HHgeQrW2N48LfXrA..|' \
  wps-365/opt/kingsoft/wps-office/office6/wtool/oem.ini \
  wps-365/opt/kingsoft/wps-office/office6/cfgs/oem.ini
 
-sed -i '/^set +e /,$d' wps-365/DEBIAN/{postinst,postrm,preinst,prerm}
+sed -i '/^set +e /,$d' wps-365/DEBIAN/postinst \
+ wps-365/DEBIAN/postrm \
+ wps-365/DEBIAN/preinst \
+ wps-365/DEBIAN/prerm
 
 sed -i -e "s/'wps-office-uninstall.desktop' //g" -e "s/'wps-office-officeassistant.desktop' //g" wps-365/DEBIAN/prerm
 
-sed -i '/^Recommends: ttf-mscorefonts-installer/d' wps-365/DEBIAN/control
+sed -i 's/ttf-mscorefonts-installer/wps-office-fonts/' wps-365/DEBIAN/control
 
 size=$(du -ks wps-365 --exclude=DEBIAN | cut -f1)
 sed -ri "s/^Installed-Size.*$/Installed-Size: $size/g" wps-365/DEBIAN/control
